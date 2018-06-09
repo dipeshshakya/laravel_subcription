e98097a5c9a4875a38e611be3511f8d9c429bc8a
dipesh shakya <dipeshshakya106@gmail.com>
2018-06-09 08:52:17 +0545

    admin dashboard

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="col-md-3">
+    <div class="panel panel-default panel-flush">
+        <div class="panel-heading">
+            Sidebar
+        </div>
+
+        <div class="panel-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/admin') }}">
+                        Dashboard
+                    </a>
+                </li>
+
+                <li role="presentation">
+                    <a href="{{ url('/admin/role') }}">
+                        Role
+                    </a>
+                </li>
+
+                <li role="presentation">
+                    <a href="{{ url('/admin/permission') }}">
+                        Permission
+                    </a>
+                </li>
+
+                <li role="presentation">
+                    <a href="{{ url('/admin/user') }}">
+                        Users
+                    </a>
+                </li>
+
+                <li role="presentation">
+                    <a href="{{ url('/admin/posts') }}">
+                        Posts
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
