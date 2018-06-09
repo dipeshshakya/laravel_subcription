353323ef533d1227859c959bb77f1e96843b98f8
dipesh shakya <dipeshshakya106@gmail.com>
2018-06-09 09:08:55 +0545

    Role curd

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('admin/permission', 'Admin\\PermissionController');
+Route::resource('admin/role', 'Admin\\RoleController');
